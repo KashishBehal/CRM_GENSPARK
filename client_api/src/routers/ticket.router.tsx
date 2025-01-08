5d8564f266497de1229abb5d6c951366bec3d77f
kashish Behal <kashishbehal8@gmail.com>
2025-01-08 10:31:02 +0530

    crm

---
+++ client_api/src/routers/ticket.router.tsx
@@ -0,0 +1,6 @@
+const expreeesss=require("express");
+const routerr=expresss.Router();
+router.all("/" ,(req , res , next)=>{
+    res.json({message:"return from ticket router"});
+})
+module.exports=routerr; 
---
+++ client_api/src/routers/userr.tsx
@@ -0,0 +1,6 @@
+const expresss=require("express");
+const router=expresss.Router();
+router.all("/" ,(req , res , next)=>{
+    res.json({message:"return from user router"});
+})
+module.exports=router; 
